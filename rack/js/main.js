005f1a1548a865dd55f6ae764d916e3377414244
Fairydusti <rantala.marko.j@student.uta.fi>
2015-11-12 20:40:21 +0200

    intro layout

--- rack/js/main.js
+++ rack/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
    var introtext = document.getElementById('intro');
    console.log("main.js infotext");
    //window.alert("KEIJOO");
-   TweenMax.to(introtext, 1.65, {marginLeft: '38.2%'});
+   TweenMax.to(introtext, 1.65, {marginLeft: '29.03%'});
 });
 
 //Script for handling the anchor scrolling
