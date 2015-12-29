41dd8f46830216fd272ae7dbecad7d00a21ef98b
Fairydusti <rantala.marko.j@student.uta.fi>
2015-12-29 12:53:36 +0200

    Some new style changes and layout, deleted red jackets, added placeholder image

--- rack/js/main.js
+++ rack/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
    var introtext = document.getElementById('intro');
    console.log("main.js infotext");
    //window.alert("KEIJOO");
-   TweenMax.to(introtext, 1.35, {marginLeft: '29.03%'});
+  // TweenMax.to(introtext, 1.35, {marginLeft: '29.03%'});
 });
 
 //Script for handling the anchor scrolling
