8a297f0eee83aae63121522b6f95ad3b155ed49c
Fairydusti <rantala.marko.j@student.uta.fi>
2016-01-08 16:14:12 +0200

    added content to intro and weto html

--- rack/js/main.js
+++ rack/js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-   var introtext = document.getElementById('intro');
+   var introtext = document.getElementById('intro-pic');
    console.log("main.js infotext");
    //window.alert("KEIJOO");
-  // TweenMax.to(introtext, 1.35, {marginLeft: '29.03%'});
+  //TweenMax.to(introtext, 1.35, {opacity: '100'});
 });
 
 //Script for handling the anchor scrolling
@@ -27,33 +27,44 @@ $(document).ready(function() {
     console.log(elementsId);
     var result="";
     if(elementsId == "skill-java"){
-      //window.alert("Keijo2");
-      result = "g: how to java";
+      result = "My first programming language. Basic knowledge of data structures, Swing-toolkit and database programming. Used JavaEE (Maven/Struts2) in one project.";
     }
     else if(elementsId =="skill-js"){
-      result = "Familiar with Bootstrap framework. jQuery is the strongest library. VanillaJS is also one of my strengths, because Java background. Currently trying to get know better ReactJS and NodeJS.";
+      result = "Familiar with Bootstrap framework. jQuery is the strongest library. VanillaJS is also one of my strengths, because my Java background. Currently trying to get know better ReactJS and NodeJS.";
     }
     else if(elementsId =="skill-html"){
       result = "Familiar with Bootstrap framework. Strong understanding of HTML-elements";
     }
     else if(elementsId =="skill-scss"){
-      result = "Familiar with Sass-preprocessing language and currently using it. Trying to get know better mixins.";
+      result = "Learned Sass-preprocessing language and currently using it. Trying to get know better mixins.";
+    }
+    else if(elementsId =="skill-php"){
+      result = "Basic knowledge of PHP. Being a part of a project where used Laravel-framework.";
+    }
+
+    else if(elementsId =="skill-cpp"){
+      result = "Knowledge of syntax. Participated C++ course.";
+    }
+    else if(elementsId =="skill-c"){
+      result = "Knowledge of syntax. Participated C course.";
+    }
+    else if(elementsId =="skill-psql"){
+      result = "Studied and used in our university courses. Used in one project";
+    }
+    else if(elementsId =="skill-msql"){
+      result = "Knowledge of syntax. Used in one project.";
+    }
+    else if(elementsId =="skill-xml"){
+      result = "Knowledge of syntax. Used Database programming course.";
+    }
+    else if(elementsId =="skill-json"){
+      result = "Knowledge of syntax. Used in two project.";
     }
     else{
       result = "Coming soon";
     }
     $(infofield).text(result);
   });
-  $('.portfolio-polaroid')       
-        .mouseover(function() { 
-            var src = $(this).children('img').attr("src").match(/[^\.]+/) + "-thumb.png";
-            $(this).attr("src", src);
-        })
-        .mouseout(function() {
-            var src = $(this).attr("src").replace("-thumb.png", ".png");
-            $(this).attr("src", src);
-        });
 });
 //});
-
 
